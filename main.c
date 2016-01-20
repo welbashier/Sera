28eab4a9642834dfdd9b3920a1a10cc9724083a0
Waleed Elbashier <welbashier@hotmail.com>
2016-01-19 20:58:46 -0600

    The second commit

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
-	printf("Hello, world!\n");
+	char name[255];
+
+	printf("Enter your name: ");
+	fgets(name, 255, stdin);
+
+	printf("Hello %s!\n", name);
 	return 0;
 }
